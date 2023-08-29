7ffdfce8ab138d985a22fe2f97808a0753cd7ece
DESKTOP-TTU94SC\81806 <tomo.airi.0526@gmail.com>
2023-08-29 11:40:58 +0900

    commit

--- Spring_project/src/main/java/com/example/demo/minus/service/MinusService.java
+++ Spring_project/src/main/java/com/example/demo/minus/service/MinusService.java
@@ -2,13 +2,11 @@ package com.example.demo.minus.service;
 
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class MinusService {
-	
-		
-		public  int minus(int num1, int num2) {
-			return num1 - num2;
-		}
+
+	public int minus(int num1, int num2) {
+		return num1 - num2;
+	}
 
 }
