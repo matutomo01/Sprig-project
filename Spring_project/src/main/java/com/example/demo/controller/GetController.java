1b5c0a5036662f5d45edaa3226371deb8afe4446
DESKTOP-TTU94SC\81806 <tomo.airi.0526@gmail.com>
2023-08-29 09:53:48 +0900

    commit

--- Spring_project/src/main/java/com/example/demo/controller/GetController.java
+++ Spring_project/src/main/java/com/example/demo/controller/GetController.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
  
 @Controller
@@ -15,7 +16,7 @@ public class GetController {
 		return "signUp.html";
 	}
  
-	@GetMapping("register")
+	@PostMapping("register")
 	public String doGet(@RequestParam("name") String name, @RequestParam("birth") LocalDate birth, Model model) {
  
 		model.addAttribute("name", name);
